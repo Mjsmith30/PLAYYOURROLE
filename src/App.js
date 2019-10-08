e5f3b5e3c22ff26218bf29c97f778c694e82cdc1
Marques Smith <marquesjs21@yahoo.com>
2019-10-08 14:26:10 -0700

    errors

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+// import logo from './logo.svg';
 import './App.css';
+import NavBar from './NavBar/NavBar.jsx'
+import { Route, Switch } from 'react-router-dom';
+import userService from './utils/userService';
+import SignupPage from './pages/SignupPage';
+import LoginPage from './Login/LoginPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {user: userService.getUser()};
+  }
+
+
+  handleLogout = () => {
+    userService.logout();
+    this.setState({ user: null });
+  }
+  handleSignupOrLogin = () => {
+    this.setState({user: userService.getUser()});
+  }
+
+  
+
+
+render(){
+    
+    return (
+    <div>
+        <header className= 'header'>Play Your Role</header>
+      <Switch>
+          <Route exact path='/signup' render={({ history }) => 
+            <SignupPage
+              history={history}
+              handleSignupOrLogin={this.handleSignupOrLogin}
+            />
+          }/>
+          <Route exact path='/login' render={({ history }) => 
+            <LoginPage
+            history={history}
+            handleSignupOrLogin={this.handleSignupOrLogin}
+
+            />
+          }/>
+         
+     
+      <NavBar />
+      </Switch>
+
     </div>
-  );
+  )
+        }
 }
+
+
 
 export default App;
